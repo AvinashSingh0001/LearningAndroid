1405ba335ec09d05f7c08c11c4d6a45c3ca51ea2
Fun6789 <aksdav05@gmail.com>
2024-06-29 02:02:17 +0530

    Understand about Splash Screen.

---
+++ SplashScreen/app/src/main/java/com/example/splashscreen/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.splashscreen
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+         supportActionBar?.hide()
+        Handler().postDelayed({
+            var intent= Intent(this,MainActivity2::class.java)
+            startActivity(intent)
+        },3000)
+    }
+}
