55ffb44f2cfa2660ee3c7fe6d0257ef26d91c65d
Fun6789 <aksdav05@gmail.com>
2024-07-04 19:38:30 +0530

    completed Sign In Activity.

--- DatabaseConnectivity/app/src/main/java/com/example/databaseconnectivity/SecondActivity.kt
+++ DatabaseConnectivity/app/src/main/java/com/example/databaseconnectivity/SecondActivity.kt
@@ -1,6 +1,8 @@
 package com.example.databaseconnectivity
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,8 +11,16 @@ import androidx.core.view.WindowInsetsCompat
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_second)
+        val mail= intent.getStringExtra(SignInActivity.KEY1)
+        val name = intent.getStringExtra(SignInActivity.KEY2)
+        val id= intent.getStringExtra(SignInActivity.KEY3)
 
+        val name1= findViewById<TextView>(R.id.textView)
+        name1.text= "Welcome, "+name.toString()
+        val mail1= findViewById<TextView>(R.id.mail)
+        mail1.text= mail.toString()
+        val id1= findViewById<TextView>(R.id.userid)
+        id1.text=id.toString()
     }
 }
--- DatabaseConnectivity/app/src/main/java/com/example/databaseconnectivity/SignInActivity.kt
+++ DatabaseConnectivity/app/src/main/java/com/example/databaseconnectivity/SignInActivity.kt
@@ -14,6 +14,11 @@ import com.google.firebase.database.FirebaseDatabase
 
 class SignInActivity : AppCompatActivity() {
     lateinit var database:DatabaseReference
+    companion object{
+        const val KEY1= "com.example.databaseconnectivity.mail"
+        const val KEY2= "com.example.databaseconnectivity.name"
+        const val KEY3= "com.example.databaseconnectivity.id"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -26,9 +31,13 @@ class SignInActivity : AppCompatActivity() {
             val userName= user.text.toString()
             val password= pass.text.toString()
             if(userName.isNotEmpty()){
-                readData(userName,password)
+                if(password.isNotEmpty()) {
+                    readData(userName, password)
+                }else{
+                    Toast.makeText(this,"Please enter your password.",Toast.LENGTH_SHORT).show()
+                }
             }else{
-                Toast.makeText(this,"Please enter right User Id",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this,"Please enter your User Id",Toast.LENGTH_SHORT).show()
             }
         }
     }
@@ -39,8 +48,16 @@ database=FirebaseDatabase.getInstance().getReference("Users")
             if (it.exists()){
                 database.child(password).get().addOnSuccessListener{
                   if (it.exists()){
-                      intent= Intent(applicationContext,SecondActivity::class.java)
+                      val name =it.child("name").value
+                      val email= it.child("email").value
+                      val uniqueID= it.child("id").value
+
+                      val intent= Intent(this,SecondActivity::class.java)
+                      intent.putExtra(KEY1,email.toString())
+                      intent.putExtra(KEY2,name.toString())
+                      intent.putExtra(KEY3,uniqueID.toString())
                       startActivity(intent)
+
                   }else{
                       Toast.makeText(this,"Please enter correct password.",Toast.LENGTH_SHORT).show()
                   }
