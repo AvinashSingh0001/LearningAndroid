3cda0d36881ecb9b9615bc0834cc793787e8a1f3
Fun6789 <aksdav05@gmail.com>
2024-07-08 00:50:09 +0530

    Rectify some mistakes related to Dialogue box implementation.

--- DialogueBox/app/src/main/java/com/example/dialoguebox/MainActivity.kt
+++ DialogueBox/app/src/main/java/com/example/dialoguebox/MainActivity.kt
@@ -33,16 +33,16 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.btn2.setOnClickListener {
-            val array = arrayOf("Gulab Jamun", "Mysore Pak", "Rasmalai", "Kaju Katli")
+            val options = arrayOf("Gulab Jamun", "Mysore Pak", "Rasmalai", "Kaju Katli")
             val builder2 = AlertDialog.Builder(this)
-            var choice= array[0]
-            builder2.setTitle("Which is your favourite")
-            builder2.setSingleChoiceItems(array, 0, DialogInterface.OnClickListener { dialog, i ->
-                choice= array[i]
-                Toast.makeText(this, "You have select ${array[i]}",  Toast.LENGTH_SHORT).show()
+            var choice= options[0]
+            builder2.setTitle("Which is your favourite Sweet")
+            builder2.setSingleChoiceItems(options, 0, DialogInterface.OnClickListener{ dialog, i ->
+                choice= options[i]
+                Toast.makeText(this, "You have select ${options[i]}",  Toast.LENGTH_SHORT).show()
             })
 
-            builder2.setMessage("choose your favourite sweet")
+
             builder2.setPositiveButton("Yes", DialogInterface.OnClickListener { dialogInterface, i ->
                 binding.choice.text = choice
             })
@@ -54,17 +54,23 @@ class MainActivity : AppCompatActivity() {
         }
         binding.btn3.setOnClickListener {
             val array = arrayOf("Gulab Jamun", "Mysore Pak", "Rasmalai", "Kaju Katli")
+            val choice= mutableListOf<String>()
             val builder3 = AlertDialog.Builder(this)
 
-            builder3.setTitle("Which is your favourite")
+            builder3.setTitle("Please select your favourite sweets")
             builder3.setMultiChoiceItems(array, null,DialogInterface.OnMultiChoiceClickListener { dialog, i, b ->
+                if (b) {
+                    choice.add(array[i])
+                } else {
+                    choice.remove(array[i])
+                }
 
-                Toast.makeText(this,"You have selected ${array[i]}", Toast.LENGTH_SHORT).show()
+
             })
 
-            builder3.setMessage("choose your favourite sweet")
-            builder3.setPositiveButton("Submit", DialogInterface.OnClickListener { dialogInterface, i ->
 
+            builder3.setPositiveButton("Submit", DialogInterface.OnClickListener { dialogInterface, i ->
+               binding.choice1.text = choice.toString()
             })
 
             builder3.setNegativeButton("CAncel", DialogInterface.OnClickListener { dialogInterface, i ->
